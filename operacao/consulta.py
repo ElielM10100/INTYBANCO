b672f9a4e184c26bc1f357619c0af3988b826241
ElielNicollas <119982627+ElielM10100@users.noreply.github.com>
2023-08-31 22:12:07 -0300

    Add files via upload
    fiz um BANCO DIGITAL USANDO PYTHON

---
+++ operacao/consulta.py
@@ -0,0 +1,11 @@
+from banco import obterconta,banco
+def consultasaldo(conta:int)-> None:
+    cliente = obterconta(conta)
+    if cliente:
+        print(f"saldo:{cliente['saldo']}")
+    else:
+     print("conta nao existe!!")
+
+if __name__=='__main__':
+    consultasaldo(1)
+    print(banco)
